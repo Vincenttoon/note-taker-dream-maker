e6de73c1eec3fdb8ae2171c89ee342e0dbfc571c
Vincent Toon <vincenttoon22@gmail.com>
2022-09-27 12:33:36 -0500

    mvp, it's functioning

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,23 +1,22 @@
-const router = require('express').Router();
-const fs = require('fs');
-const { notes } = require('../../db/db.json');
-const { createNote, validateNote } = require('../../lib/notes');
+const router = require("express").Router();
+const fs = require("fs");
+const notes = require("../../db/db.json");
+const { createNote, validateNote } = require("../../lib/notes");
 
+router.get("/notes", (req, res) => {
+  let results = notes;
+  // should read the db.json file and return all saved notes as JSON.
 
-router.get('api/notes', (req, res) => {
-    let results = notes
-    // should read the db.json file and return all saved notes as JSON.
-    if (req.query) {
-        results = validateNote(req.query, results);
-    }
-    res.json(results);
+  res.json(results);
 });
 
-router.post("/api/notes", (req, res) => {
-    // should receive a new note to save on the request body,
-    // add it to the json file,
-    // return the new note to the client
-    console.log('read above')
+router.post("/notes", (req, res) => {
+  let allNotes = createNote(req.body, notes);
+
+  res.json(allNotes);
+  // should receive a new note to save on the request body,
+  // add it to the json file,
+  // return the new note to the client
 });
 
 module.exports = router;
