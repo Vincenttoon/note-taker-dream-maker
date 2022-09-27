a6bb83c2760af8a89d7c9b5084b140deca7f929b
Vincent Toon <vincenttoon22@gmail.com>
2022-09-27 17:19:56 -0500

    delete working code finished

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,11 +1,10 @@
 const router = require("express").Router();
 const fs = require("fs");
 const notes = require("../../db/db.json");
-const { createNote, validateNote } = require("../../lib/notes");
+const { createNote, deleteNote } = require("../../lib/notes");
 
 router.get("/notes", (req, res) => {
   let results = notes;
-  // should read the db.json file and return all saved notes as JSON.
 
   res.json(results);
 });
@@ -14,9 +13,11 @@ router.post("/notes", (req, res) => {
   let allNotes = createNote(req.body, notes);
 
   res.json(allNotes);
-  // should receive a new note to save on the request body,
-  // add it to the json file,
-  // return the new note to the client
 });
+
+router.delete("/notes/:id", (req, res) => {
+    deleteNote(notes, req.params.id);
+    res.json(notes);
+})
 
 module.exports = router;
